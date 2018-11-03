57935cf817e48db318ee08c58bfaf69e12be2f19
Vincent Gay <glurbi@gmail.com>
2018-11-03 20:49:26 +0100

    and more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {MaterialModule} from '../material-module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import {MaterialModule} from '../material-module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FormsModule,
     MaterialModule,
   ],
   providers: [],
